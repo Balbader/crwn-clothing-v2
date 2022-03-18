c1f80b5a400287e0b8c55820bef37e5029857250
Balbader <15971200+Balbader@users.noreply.github.com>
2022-03-18 19:51:08 +0100

    add home route to app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Home from './components/routes/home/home.component';
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
-      </Route>
+      <Route path='/' element={<Home />} />
     </Routes>
   );
 };
